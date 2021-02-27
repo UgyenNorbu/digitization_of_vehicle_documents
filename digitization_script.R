dcf96facc1b094442345acccac059ebf3d895430
Ugyen Norbu <66914661+UgyenNorbu@users.noreply.github.com>
2021-02-27 23:12:32 +0600

    1st

---
+++ digitization_script.R
@@ -0,0 +1,10 @@
+library(tidyverse)
+library(readxl)
+
+# 1. DATA IMPORT ----------------------------------------------------------
+DL_new <- readxl::read_xls("digitization_of_vehicle_documents/Data_digitization/DL_new.xls")
+
+
+# 2.  EDA -----------------------------------------------------------------
+
+
